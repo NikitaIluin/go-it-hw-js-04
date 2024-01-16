921dfdb946ab6d6caa92c487b5a9b7fa8d27d8bd
NikitaIluin <142826642+NikitaIluin@users.noreply.github.com>
2024-01-16 13:29:07 +0200

    commit

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+// task-3.js
+
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    getInfo: function () {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+    changeUsername: function (newName) {
+        this.username = newName;
+    },
+    updatePlayTime: function (hours) {
+        this.playTime += hours;
+    },
+};
+
+// Перевірка роботи методів за допомогою виводу у консоль
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
